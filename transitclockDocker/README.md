bd3495ed15817b570a32537633a8c9f3571d2665
Joshua Santiago <joshua.santiago14@upr.edu>
2020-11-17 23:39:17 -0400

    fsda

---
+++ transitclockDocker/README.md
@@ -0,0 +1,34 @@
+# transitime-docker
+
+Things to make transitime go:
+
+- Ubuntu
+- sudo apt-get install git
+- git clone https://github.com/scrudden/transitime-docker.git
+- curl -sSL https://get.docker.com/ | sh  (i.e. install docker)
+- Configure agency details in the go.sh script. Here you set the agency name, agency id** (as in GTFS feed), GTFS feed location and GTFS-realtime vehicle location url.
+- ./go.sh
+
+**AgencyId is optional in GTFS so just set to 1 if none specified.
+
+The go script will build the transitime container (takes a long time), start the postgres db, create the tables,
+push the gtfs data into the db, create an API key and then start the api service and web user interface service. 
+
+To view web interface
+```
+http://[ipaddress]:8080/web
+```
+To view api
+```
+http://[ipaddress]:8080/api
+```
+
+You can get the ip address by running
+```
+docker-machine ip [machine name]
+
+docker-machine ip default
+```
+
+
+
---
+++ transitclockDocker/go.sh
@@ -0,0 +1,34 @@
+export PGPASSWORD=transitclock
+
+docker stop transitclock-db
+docker stop transitclock-server-instance
+
+docker rm transitclock-db
+docker rm transitclock-server-instance
+
+docker rmi transitclock-server
+
+docker build --no-cache -t transitclock-server \
+--build-arg TRANSITCLOCK_PROPERTIES="config/transitclock.properties" \
+--build-arg AGENCYID="1" \
+--build-arg AGENCYNAME="TIM" \
+--build-arg GTFS_URL="https://drive.google.com/uc?export=download&id=19HFNi4crudjKT4aoDQ6khCIab02xATFp" \
+--build-arg GTFSRTVEHICLEPOSITIONS='http://45.55.84.20:8080/api/positions' .
+
+docker run --name transitclock-db -p 5432:5432 -e POSTGRES_PASSWORD=$PGPASSWORD -d postgres:9.6.3
+
+docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD -v ~/logs:/usr/local/transitclock/logs/ transitclock-server check_db_up.sh
+
+docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD -v ~/logs:/usr/local/transitclock/logs/ transitclock-server create_tables.sh
+
+docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD -v ~/logs:/usr/local/transitclock/logs/ transitclock-server import_gtfs.sh
+
+docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD -v ~/logs:/usr/local/transitclock/logs/ transitclock-server create_api_key.sh
+
+docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD -v ~/logs:/usr/local/transitclock/logs/ transitclock-server create_webagency.sh
+
+#docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server ./import_avl.sh
+
+#docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server ./process_avl.sh
+
+docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD  -v ~/logs:/usr/local/transitclock/logs/ -v ~/ehcache:/usr/local/transitclock/cache/ -p 8080:8080 transitclock-server  start_transitclock.sh
